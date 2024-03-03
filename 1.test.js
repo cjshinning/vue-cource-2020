a0069a23800d901996d714ef36bbc9e1aa655538
cjshinningg <784319417@qq.com>
2024-03-03 12:23:53 +0800

    6.指令用法

--- 1.test.js
+++ 1.test.js
@@ -5,8 +5,12 @@ let t1 = `<div v-if="true">hello</div>
 
 let t2 = `<div v-show="true">hello</div>`;
 
-console.log(compiler.compile(t2).render);
+let t3 = `<div v-for="(a,index) of arr" :key="index" v-if="a%2===0"></div>`;
+
+console.log(compiler.compile(t3).render);
 
 // with(this){return (true)?_c('div',[_v("hello")]):_c('div',[_v("world")])}
 
 // with(this){return _c('div',{directives:[{name:"show",rawName:"v-show",value:(true),expression:"true"}]},[_v("hello")])}
+
+// with(this){return _l((arr),function(a,index){return (a%2===0)?_c('div',{key:index}):_e()})}
